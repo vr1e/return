457029b3c2b7fbd782ce72fe6436505ad768dc2d
Nikola Vrhovac <vrllee@gmail.com>
2020-05-14 17:47:12 +0200

    Add basic transliterate

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Particles from 'react-particles-js';
-import Return from './return.png';
 import particleConfig from './particleConfig';
 import './index.scss';
 
+import Home from './components/Home';
+import Transliterate from './components/Transliterate';
+
 const App = () => {
 	return (
-		<>
+		<BrowserRouter>
 			<Particles params={particleConfig} />
-			<div className='text'>
-				<h1>Return</h1>
-				<img src={Return} alt='Return' width="312" height="73" />
-				<p>Hacking the web since 2017.</p>
-			</div>
-		</>
+			{/* <Route path='/cyrillicconvert' render={Transliterate} />
+			<Route path='/' render={Home} exact /> */}
+
+			{/* <Transliterate></Transliterate> */}
+			<ul className='nav'>
+				<li className='nav-item'>
+					<Link to='/' className='nav-link'>
+						Home
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link to='/cyrillicconvert' className='nav-link'>
+						Cyr&lt;&gt;Latin
+					</Link>
+				</li>
+			</ul>
+			<Switch>
+				<Route path='/cyrillicconvert'>
+					<Transliterate />
+				</Route>
+				<Route path='/'>
+					<Home />
+				</Route>
+			</Switch>
+		</BrowserRouter>
 	);
 };
 
