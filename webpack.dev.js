f0d375f410e9c1372dfe2d5b79bbd2e8a1b744dc
Nikola Vrhovac <vrllee@gmail.com>
2020-05-14 12:59:15 +0200

    Npm versions bump

--- webpack.dev.js
+++ webpack.dev.js
@@ -141,7 +141,7 @@ module.exports = {
 		contentBase: path.join(__dirname, 'src'),
 		compress: true,
 		port: 9000,
-		historyApiFallback: true,
-		stats: 'minimal'
+		historyApiFallback: true
+		// stats: 'minimal'
 	}
 };
