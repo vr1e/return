e01042216d0f0c3a346d53de36ecd7da04286725
Nikola Vrhovac <Nikola.Vrhovac@gvcgroup.com>
2021-09-22 10:37:56 +0200

    hover for tech icons

--- utils/get-data.ts
+++ utils/get-data.ts
@@ -1,6 +1,6 @@
 export async function getFireData(dataPath: string = 'cv') {
 	const response = await fetch(
-		`${process.env.NEXT_PUBLIC_DB_HOST}/${dataPath}.json`
+		`${process.env.DB_HOST}/${dataPath}.json`
 	);
 
 	const data = await response.json();
