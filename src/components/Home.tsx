d4ba39e98d628391d67dae88b831ff9f832e3a28
Nikola Vrhovac <vrllee@gmail.com>
2022-10-16 14:35:42 +0200

    fix homepage image url, delete old assets

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
+import returnLogo from '../assets/return.png';
+
 export default function Home() {
 	return (
 		<div className='text'>
 			<h1>Return</h1>
-			<img src='../assets/return.png' alt='Return' width='312' height='73' />
+			<img src={returnLogo} alt='Return' width='312' height='73' />
 			<p>Hacking the web since 2017.</p>
 		</div>
 	);
